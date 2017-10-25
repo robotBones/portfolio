d25df3c1ceaaa911300c030968d8f92e9893c2f4
Kim Marquardt <kim.marquardt@gmail.com>
2017-10-24 20:46:32 -0700

    Remove unused views and routes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var stylus = require('stylus');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -29,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/blog', express.static(path.join(__dirname, 'blog/public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
