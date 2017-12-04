be6cfc6634ecdbaa9bb3bb128c21e3b906fa5045
Kim Marquardt <kim.marquardt@gmail.com>
2017-12-03 16:06:27 -0800

    Refactor class toggling for loading screen

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -3,10 +3,8 @@
 (function() {
   function killLoadingScreen() {
     var el = document.getElementById('loading-screen');
-    var classes = el.className.split(' ');
-    if (classes.indexOf('stop') === -1) {
-      classes.push('stop')
-      el.className = classes.join(' ').trim();
+    if (!el.classList.contains('stop')) {
+      el.classList.add('stop');
     }
   }
 
